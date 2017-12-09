0cfa9cd6e8629da6ca23bbdf1508e9068cc63da6
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2017-12-09 17:35:55 -0500

    finished conv_net_tf.py

--- deepLearningNano/conv-net/conv_net_tf.py
+++ deepLearningNano/conv-net/conv_net_tf.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -10,7 +11,6 @@ N_CLASSES = 10
 DROPOUT = 0.75
 
 test_valid_size = 256
-
 weights = {'Wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
            'Wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
            'Wd':  tf.Variable(tf.random_normal([7 * 7 * 64, 1024])),
@@ -39,12 +39,12 @@ def conv_net(X, W, b, dropout):
 
     # L3 FC: 7x7x64 -> 1024
     fc1 = tf.reshape(conv2, [-1, W['Wd'].get_shape().as_list()[0]])
-    fc1 = fc1 * W['Wd'] + b['bd']
+    fc1 = tf.matmul(fc1, W['Wd']) + b['bd']
     fc1 = tf.nn.relu(fc1)
     fc1 = tf.nn.dropout(fc1, dropout)
 
     # Out 1024 -> 10
-    out = fc1 * W['W_out'] + b['b_out']
+    out = tf.matmul(fc1, W['W_out']) + b['b_out']
     return out
 
 # Graph inputs
@@ -62,3 +62,42 @@ optimizer = tf.train.GradientDescentOptimizer(learning_rate=ETA).minimize(cost)
 
 # Accuracy
 correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# Init
+init = tf.global_variables_initializer()
+
+
+# Launch graph
+with tf.Session() as s:
+    s.run(init)
+    acc = []
+
+    for epoch in range(EPOCHS):
+        for batch in range(mnist.train.num_examples // BATCH_SIZE):
+            batch_X, batch_y = mnist.train.next_batch(BATCH_SIZE)
+            s.run(optimizer,
+                  feed_dict={X: batch_X, y: batch_y, keep_prob: DROPOUT})
+
+            # Batch loss, accuracy
+            loss = s.run(cost,
+                         feed_dict={X: batch_X, y: batch_y, keep_prob: 1.})
+            valid_acc = s.run(
+                accuracy,
+                feed_dict={X: mnist.validation.images[:test_valid_size],
+                           y: mnist.validation.labels[:test_valid_size],
+                           keep_prob: 1.})
+
+            acc.append(valid_acc)
+            print('Epoch {:>2}, Batch {:>3} - Loss: {:>10.4f} '
+                  'Valid. Acc: {:.6f}'.format(
+                      epoch + 1, batch + 1, loss, valid_acc))
+
+    # Test Accuracy
+    test_acc = s.run(accuracy,
+                     feed_dict={X: mnist.test.images[:test_valid_size],
+                                y: mnist.test.labels[:test_valid_size],
+                                keep_prob: 1.})
+    print('Test Accuracy: {}'.format(test_acc))
+    plt.plot(acc, 'r-')
+    plt.show()
