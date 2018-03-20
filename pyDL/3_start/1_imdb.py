6f8cadf1d560e999f7fd452cba5a3e50b894004b
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-03-20 08:47:08 -0700

    adds 2

--- pyDL/3_start/1_imdb.py
+++ pyDL/3_start/1_imdb.py
@@ -1,9 +1,16 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from keras.datasets import imdb
 from keras.layers import Dense
+from keras.metrics import binary_accuracy
 from keras.models import Sequential
+from keras.optimizers import RMSprop
 
 N_WORDS = 10000 # keep only the most frequent N_WORDS words
+N_VALID = 10000
+EPOCHS = 20
+BATCH = 512
+
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=N_WORDS)
 
 print('Sample datum:', X_train[0])
@@ -13,6 +20,7 @@ print('Max word index:', max([max(seq) for seq in X_train])) # <= N_WORDS - 1
 # Decode back to English
 word_index = imdb.get_word_index()
 reverse_word_index = {v: k for k, v in word_index.items()}
+
 # Offset by 3 bc 0, 1, 2 reserved for 'padding', 'start', and 'unk'
 print(' '.join([reverse_word_index.get(i - 3, '?') for i in X_train[0]]))
 
@@ -28,9 +36,29 @@ X_test  = vectorize_sequences(X_test)
 y_train = np.asarray(y_train).astype('float32')
 y_test  = np.asarray(y_test).astype('float32')
 
+# Split off validation data
+X_valid = X_train[:N_VALID]
+X_train = X_train[N_VALID:]
+y_valid = y_train[:N_VALID]
+y_train = y_train[N_VALID:]
+
 model = Sequential()
 model.add(Dense(16, activation='relu', input_shape=(N_WORDS,)))
 model.add(Dense(16, activation='relu'))
 model.add(Dense(1,  activation='sigmoid'))
-model.summary()
-          
+print(model.summary())
+
+opt = RMSprop(lr=0.001)
+model.compile(
+    optimizer=opt, loss='binary_crossentropy', metrics=[binary_accuracy])
+
+history = model.fit(X_train,
+                    y_train,
+                    epochs=EPOCHS,
+                    batch_size=BATCH,
+                    validation_data=(X_valid, y_valid))
+plt.plot(history.history['loss'], 'k-', label='train')
+plt.plot(history.history['val_loss'], 'r-', label='valid')
+plt.xlabel('Loss (binary cross-entropy)')
+plt.legend(loc='best')
+plt.show()
---
+++ pyDL/3_start/2_reuters.py
@@ -0,0 +1,68 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.datasets import reuters
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.utils.np_utils import to_categorical
+
+N_WORDS = 10000
+N_CLASSES = 46
+VALID_SIZE = 1000
+EPOCHS = 20
+BATCH = 512
+
+# Training data is words as numeric representations; lables are 0-45 topic ids
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(
+    num_words=N_WORDS)
+print(train_data[10])
+word_index = reuters.get_word_index()
+reverse_word_index = {v: k for k, v in word_index.items()}
+decoded_newswire = ' '.join([reverse_word_index.get(i - 3, '?')
+                             for i in train_data[10]])
+print(decoded_newswire)
+
+
+def one_hot(sequences, dimension=N_WORDS):
+    results = np.zeros([len(sequences), dimension])
+    for i, seq in enumerate(sequences):
+        results[i, seq] = 1.
+    return results
+
+X_train = one_hot(train_data)
+X_test  = one_hot(test_data)
+Y_train = one_hot(train_labels, dimension=N_CLASSES)
+Y_test  = one_hot(test_labels,  dimension=N_CLASSES)
+
+# Same as:
+#X_train = to_categorical(train_data)
+#X_test  = to_categorical(test_data)
+#Y_train = to_categorical(train_labels)
+#Y_test  = to_categorical(test_labels)
+
+
+X_valid = X_train[:VALID_SIZE]
+X_train = X_train[VALID_SIZE:]
+Y_valid = Y_train[:VALID_SIZE]
+Y_train = Y_train[VALID_SIZE:]
+
+mod = Sequential()
+mod.add(Dense(64, activation='relu', input_shape=(N_WORDS,)))
+mod.add(Dense(64, activation='relu'))
+mod.add(Dense(N_CLASSES, activation='softmax'))
+
+mod.compile(
+    optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+history = mod.fit(X_train,
+                  Y_train,
+                  epochs=EPOCHS,
+                  batch_size=BATCH,
+                  validation_data=(X_valid, Y_valid))
+
+plt.plot(history.history['loss'], 'k-', label='Train')
+plt.plot(history.history['val_loss'], 'r-', label='Valid')
+plt.ylabel('Categorical Cross-Entropy')
+plt.legend()
+plt.show()
+
+preds = mod.predict(X_test)
