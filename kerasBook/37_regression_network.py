9d972b50413ea7640956c4ba78f0030b9ef52914
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-04-12 08:32:21 -0700

    +37 (regression ex.)

--- kerasBook/37_regression_network.py
+++ kerasBook/37_regression_network.py
@@ -10,6 +10,8 @@ from sklearn.preprocessing import StandardScaler
 
 DATA_DIR = './data/'
 INFILE = 'AirQualityUCI.csv'
+EPOCHS = 20
+BATCH = 10
 
 data = pd.read_csv(DATA_DIR + INFILE, sep=';', decimal=',', header=0)
 data = data.drop(['Date', 'Time', 'Unnamed: 15', 'Unnamed: 16'], axis=1)
@@ -35,3 +37,27 @@ benzene = Dense(1, kernel_initializer='glorot_uniform')(x)
 mod = Model(inputs=[readings], outputs=[benzene])
 print(mod.summary())
 mod.compile(loss='mse', optimizer='adam')
+
+history = mod.fit(
+    X_train, y_train, batch_size=BATCH, epochs=EPOCHS, validation_split=0.2)
+
+raw_pred = mod.predict(X_test).flatten()
+for i in range(10):
+    label = y_test[i]*X_sds[3] + X_means[3]
+    prediction = raw_pred[i]*X_sds[3] + X_means[3]
+    print('Benzene Concentration:\nObserved: {:.3f}, Predicted: {:.3f}'.format(
+        label, prediction))
+
+plt.plot(np.arange(y_test.shape[0]),
+         y_test * X_sds[3] / X_means[3],
+         color='k',
+         label='actual')
+plt.plot(np.arange(raw_pred.shape[0]),
+         raw_pred * X_sds[3] / X_means[3],
+         color='r',
+         label='predicted')
+plt.xlabel('Time')
+plt.ylabel('Benzene concentration')
+plt.legend()
+plt.show()
+          
