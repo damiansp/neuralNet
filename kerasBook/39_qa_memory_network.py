c417ace296e4fd69b541db7d4852ade2d12d220e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-04-17 09:49:10 -0700

    +39

--- kerasBook/39_qa_memory_network.py
+++ kerasBook/39_qa_memory_network.py
@@ -18,15 +18,21 @@ DATA_DIR = './data/fb_bAbI/en/'
 TRAIN_FILE = DATA_DIR + 'qa1_single-supporting-fact_train.txt'
 TEST_FILE = DATA_DIR + 'qa1_single-supporting-fact_test.txt'
 PAD = 0
+EMBEDDING_SIZE = 64
+LATENT_SIZE = 32
+DROPOUT = 0.3
+BATCH = 32
+EPOCHS = 50
+N_DISPLAY = 10
 
 
 def get_data(infile):
     stories, questions, answers = [], [], []
     story_text = []
-    f_in = open(TRAIN_FILE, 'rb')
+    f_in = open(infile, 'rb')
     for line in f_in:
         line = line.decode('utf-8').strip()
-        line_number, text = line.split(' ', 1)
+        _, text = line.split(' ', 1)
         if '\t' in text:
             question, answer, _ = text.split('\t')
             stories.append(story_text)
@@ -56,7 +62,7 @@ def build_vocab(train_data, test_data):
                 for answer in answers:
                     for word in nltk.word_tokenize(answer):
                         counter[word.lower()] += 1
-    word2idx = {w: (i + 1) for i, (w, _) in enumerate(counter.most_common())}
+    word2idx = {w: i + 1 for i, (w, _) in enumerate(counter.most_common())}
     word2idx['PAD'] = PAD
     idx2word = {v: k for k, v in word2idx.items()}
     return word2idx, idx2word
@@ -66,12 +72,12 @@ vocab_size = len(word2idx)
 
 
 def get_maxlens(train_data, test_data):
-    story_maxlen, question_max_len = 0, 0
+    story_maxlen, question_maxlen = 0, 0
     for stories, questions, _ in [train_data, test_data]:
         for story in stories:
             story_len = 0
             for sentence in story:
-                s_words = nltk.word_tokenize(sent)
+                s_words = nltk.word_tokenize(sentence)
                 story_len += len(s_words)
             if story_len > story_maxlen:
                 story_maxlen = story_len
@@ -81,7 +87,7 @@ def get_maxlens(train_data, test_data):
                 question_maxlen = question_len
     return story_maxlen, question_maxlen
 
-story_maxlen, question_max = get_maxlens(data_train, data_test)
+story_maxlen, question_maxlen = get_maxlens(data_train, data_test)
 
 
 def vectorize(data, word2idx, story_maxlen, question_maxlen):
@@ -94,14 +100,74 @@ def vectorize(data, word2idx, story_maxlen, question_maxlen):
         x_q = [word2idx[w.lower()] for w in nltk.word_tokenize(question)]
         X_s.append(x_s)
         X_q.append(x_q)
-        Y.append(word2indx[answer.lower()])
-    return (pad_sequence(X_s, maxlen=story_maxlen),
-            pad_sequence(X_q, maxlen=question_maxlen),
+        Y.append(word2idx[answer.lower()])
+    return (pad_sequences(X_s, maxlen=story_maxlen),
+            pad_sequences(X_q, maxlen=question_maxlen),
             np_utils.to_categorical(Y, num_classes=len(word2idx)))
 
 X_s_train, X_q_train, Y_train = vectorize(
     data_train, word2idx, story_maxlen, question_maxlen)
-X_s_test,  X_q_test,  Y_train = vectorize(
+X_s_test,  X_q_test,  Y_test = vectorize(
     data_test,  word2idx, story_maxlen, question_maxlen)
 
 
+
+# Model--------------------------------------------------------------------
+# Inputs
+story_input = Input(shape=(story_maxlen,))
+question_input = Input(shape=(question_maxlen,))
+
+# Story encoder memory
+story_encoder = Embedding(input_dim=vocab_size,
+                          output_dim=EMBEDDING_SIZE,
+                          input_length=story_maxlen)(story_input)
+story_encoder = Dropout(DROPOUT)(story_encoder)
+
+# Question encoder
+question_encoder = Embedding(input_dim=vocab_size,
+                             output_dim=EMBEDDING_SIZE,
+                             input_length=question_maxlen)(question_input)
+question_encoder = Dropout(DROPOUT)(question_encoder)
+
+# Match between story and question
+match = dot([story_encoder, question_encoder], axes=[2, 2])
+
+# Encode story into vector space of question
+story_encoder_c = Embedding(input_dim=vocab_size,
+                            output_dim=question_maxlen,
+                            input_length=story_maxlen)(story_input)
+story_encoder_c = Dropout(DROPOUT)(story_encoder_c)
+
+# Combine match and story vectors
+response = add([match, story_encoder_c])
+response = Permute((2, 1))(response)
+
+# Combine response and question vectors
+answer = concatenate([response, question_encoder], axis=-1)
+answer = LSTM(LATENT_SIZE)(answer)
+answer = Dropout(DROPOUT)(answer)
+answer = Dense(vocab_size)(answer)
+output = Activation('softmax')(answer)
+
+mod = Model(inputs=[story_input, question_input], outputs=output)
+mod.compile(
+    optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+history = mod.fit([X_s_train, X_q_train],
+                  [Y_train],
+                  batch_size=BATCH,
+                  epochs=EPOCHS,
+                  validation_data=([X_s_test, X_q_test], [Y_test]))
+
+y_test = np.argmax(Y_test, axis=1)
+preds = mod.predict([X_s_test, X_q_test])
+preds = np.argmax(preds, axis=1)
+
+
+for i in range(N_DISPLAY):
+    story = ' '.join([idx2word[x] for x in X_s_test[i].tolist() if x != 0])
+    question = ' '.join([idx2word[x] for x in X_q_test[i].tolist()])
+    label = idx2word[y_test[i]]
+    prediction = idx2word[preds[i]]
+    print(story, question, label, prediction)
