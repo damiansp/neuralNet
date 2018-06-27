7db3f67763dea1c69d1c3c6dfa2a676ba9f71620
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-27 07:59:39 -0700

    tensors+

--- pytorch/dlPyTorch/tensors.py
+++ pytorch/dlPyTorch/tensors.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import sklearn.datasets
 import torch
@@ -15,19 +16,27 @@ vec = torch.FloatTensor([23, 24, 24.5, 26, 27.2, 23.0])
 print(vec, vec.size())
 
 # Matrices
-#boston_tensor = torch.from_numpy(boston.data)
-#print('boston_tensor:', boston_tensor.size())
-#print(boston_tensor[:2])
+boston_tensor = torch.from_numpy(boston.data)
+print('boston_tensor:', boston_tensor.size())
+print(boston_tensor[:2])
 
 # 3D Tensors
-#panda = np.array(Image.open('panda.jpg').resize((224, 224)))
-#panda_tensor = torch.from_numpy(panda)
-#panda_tensor.size()
-#plt.imshow(panda)
+me = np.array(Image.open('images/me.jpg'))#.resize((224, 224))) # 825, 743, 3
+me_tensor = torch.from_numpy(me)
+print(me_tensor.size())
+plt.imshow(me)
+plt.show()
 
 # Slicing Tensors
 sales = torch.FloatTensor(
     [1000., 323.2, 333.4, 444.5, 1000., 323.2, 333.4, 444.5])
 print(sales[:5])
 print(sales[:-5])
-plt.imshow(panda_tensor
+plt.imshow(me_tensor[:, :, 0].numpy())
+plt.show()
+plt.imshow(me_tensor[:, :, 1].numpy())
+plt.show()
+plt.imshow(me_tensor[:, :, 2].numpy())
+plt.show()
+
+
