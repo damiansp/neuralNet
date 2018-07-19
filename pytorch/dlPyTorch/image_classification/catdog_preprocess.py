e7db9d589060355377bb4a75c097f499a8c4942a
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-07-19 08:04:28 -0700

    extends

--- pytorch/dlPyTorch/image_classification/catdog_preprocess.py
+++ pytorch/dlPyTorch/image_classification/catdog_preprocess.py
@@ -7,3 +7,4 @@ simple_transform = transforms.Compose(
      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 train = ImageFolder('../data/dogs_and_cats/train/', simple_transform)
 valid = ImageFolder('../data/dogs_and_cats/valid/', simple_transform)
+
