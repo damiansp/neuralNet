f890b3a44f7b5278c07e9a352e47917e41b15bed
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-08-03 09:27:10 -0700

    rm dlPyTorch

--- pytorch/dlPyTorch/README.md
+++
@@ -1,2 +0,0 @@
-# Deep Learning with PyTorch
-<a href="https://github.com/PacktPublishing/Deep-Learning-with-PyTorch">Book Repo</a>.
--- pytorch/dlPyTorch/activations.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torch.autograd import Variable
-from torch.nn import ReLU
-
-
-sample_data = Variable(torch.Tensor([[1, 2, -1, -1]]))
-relu = ReLU()
-print(relu(sample_data))
-                       
