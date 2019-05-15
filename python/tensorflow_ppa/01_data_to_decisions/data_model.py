f5f6e9a8ca56a2aec607673df1aca71cf8bf2514
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-05-15 13:20:03 -0700

    add more tf basics

--- python/tensorflow_ppa/01_data_to_decisions/data_model.py
+++ python/tensorflow_ppa/01_data_to_decisions/data_model.py
@@ -42,4 +42,46 @@ with tf.Session() as sess:
     print(t2d.get_shape()) # (3, 3)
     print(sess.run(t2d))   # [[1 2 3][4 5 6][7 8 9]]
 
+tensor_3d = np.array([[[0, 1, 2], [3, 4, 5], [6, 7, 8]],
+                      [[2, 3, 4], [5, 6, 7], [8, 9, 0]],
+                      [[4, 5, 6], [7, 8, 9], [0, 1, 2]]])
+tensor_3d = tf.convert_to_tensor(tensor_3d, dtype=tf.float64)
+with tf.Session() as sess:
+    print(tensor_3d.get_shape()) # (3, 3, 3)
+    print(sess.run(tensor_3d))
 
+
+# Variables
+value = tf.Variable(0, name='value')
+one = tf.constant(1)
+new_value = tf.add(value, one)
+update_value = tf.assign(value, new_value)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    print(sess.run(value)) # 0
+    for _ in range(5):
+        sess.run(update_value)
+        print(sess.run(value)) # 1, 2, 3, 4, 5
+
+
+# Fetching
+const_A = tf.constant([100.])
+const_B = tf.constant([300.])
+const_C = tf.constant([3.])
+sum_ = tf.add(const_A, const_B)
+prod_ = tf.multiply(const_A, const_C)
+with tf.Session() as sess:
+    result = sess.run([sum_, prod_])
+    print(result) # [400.], [300.]
+
+
+# Feeds and Placeholders
+a = 3
+b = 2
+x = tf.placeholder(tf.float32, shape=(a, b))
+y = tf.add(x, x)
+data = np.random.rand(a, b)
+sess = tf.Session()
+print(sess.run(y, feed_dict={x: data}))
+sess.close()
