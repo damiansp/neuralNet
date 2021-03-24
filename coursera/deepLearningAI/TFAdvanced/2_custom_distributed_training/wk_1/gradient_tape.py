d87702eeedc8e7b06f726b09efd0d85aa82ae118
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-03-24 07:54:05 -0700

    grad tape++

--- coursera/deepLearningAI/TFAdvanced/2_custom_distributed_training/wk_1/gradient_tape.py
+++ coursera/deepLearningAI/TFAdvanced/2_custom_distributed_training/wk_1/gradient_tape.py
@@ -33,3 +33,48 @@ for _ in range(500):
     print(f'Est: y = {w.numpy()} + {b.numpy()}')
 
     
+def train_step(X, y):
+    with tf.GradientTape() as tape:
+        logits = model(X, training=True) # preds
+        loss = loss_object(y, logits)
+    loss_history.append(loss.numpy().mean())
+    grads = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+
+w = tf.Variable([[1.]])
+with tf.GradientTape() as tape:
+    loss = w * w                # d/dw[w^2] = 2w
+
+tape.gradient(loss, w)          # tf.Tensor([[2.]], shape=(1, 1), dtype=float32)
+
+
+x = tf.ones((2, 2))             # [[1 1]
+with tf.GradientTape() as t:    #  [1 1]]
+    t.watch(x)
+
+y = tf.reduce_sum(x)            # 1 + 1 + 1 + 1 = 4
+z = tf.square(y)                # 4^2 = 16
+dzdx = t.gradient(z, x)         # dz/dx = [[8 8]
+                                #          [8 8]]
+
+x = tf.constant(3.)
+with tf.GradientTape(persistent=True) as t:
+    t.watch(x)
+    y = x * x
+    z = y * y
+
+dzdx = t.gradient(z, x) # dz/dy dy/dx = 2y * 2x = 2x^2 * 2x = 4x^3 | @x=3: 108
+# normally tape would be consumed in prev, but persists b/c persitent=True
+dydx = t.gradient(y, x) # dy/dx = 2x | @x=3: 6
+del t # drop ref to tape (bc persistent)
+
+
+# higher order grads
+x = tf.Variable(1.)
+with tf.GradientTape() as t2:
+    with tf.GradientTape() as t1:
+        y = x ** 3                # y = x^3
+    dydx = t1.gradient(y, x)      # dy/dx = 3x^2
+d2ydx2 = t2.gradient(dydx, x)     # d2x/dx2 = x
+
